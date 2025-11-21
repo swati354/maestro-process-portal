a1d37cab10d2f07ea0a49b867b6cfda7d7e740ac
Nucleus <nucleus-bot@uipath.com>
2025-11-21 13:33:27 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add bpmn-js"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
